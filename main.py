7999b7df1b8735c4cf8bd318f157ac612ce02ab1
buntaga <52316480+buntaga@users.noreply.github.com>
2023-03-09 12:41:12 +0000

    set up templates, randomized navbar logo

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template
+import os
+import random
 
 app = Flask(__name__)
 
+imgs = os.listdir('static/nav_imgs')
+imgs = ['nav_imgs/' + file for file in imgs]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  imgrand = random.sample(imgs, k=4)
+  return render_template('home.html', imgrand=imgrand)
+
+
+@app.route("/history")
+def cdla_history():
+  imgrand = random.sample(imgs, k=4)
+  return render_template('history.html', imgrand=imgrand)
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
