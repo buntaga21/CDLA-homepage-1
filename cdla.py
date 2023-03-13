bc9106484dbd02009165c6e4a513d61277e9e767
buntaga <52316480+buntaga@users.noreply.github.com>
2023-03-13 14:39:10 +0000

    added team list to navbar, team rosters to team pages

--- cdla.py
+++ cdla.py
@@ -28,3 +28,497 @@ def champions_list():
         champions_list.append(string)
   champions_list.sort()
   return champions_list
+
+
+def team_list():
+  team_list = []
+  for team in cdla['teams']:
+    team_name = team['region'] + " " + team['name']
+    team_list.append({'name': team_name, 'tid': team['tid']})
+  team_list = sorted(team_list, key=lambda d: d['name'])
+  return team_list
+
+
+def reigning_champ_players():
+  champions_list = []
+
+  for team in cdla['teams']:
+    for season in team['seasons']:
+      if season['playoffRoundsWon'] == 4:
+        string = str(season['season']) + " " + str(team['tid'])
+        champions_list.append(string)
+
+  champions_list.sort()
+
+  reigning = champions_list[-1]
+
+  reigningTid = int(reigning[len(reigning) - 1])
+
+  print(reigningTid)
+
+  print(type(reigningTid))
+  print(len(cdla['players']))
+
+  playerChamps = []
+  awardsCount = 0
+
+  for player in cdla['players']:
+    if player['stats']:
+      for stat in player['stats']:
+        if stat['season'] == cdla['gameAttributes'][0]['season'] and stat[
+            'tid'] == reigningTid and stat['playoffs']:
+          awardsCount += 1
+          pos = player['ratings'][len(player['ratings']) - 1]['pos']
+          ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+          playerChamps.append({
+            'name':
+            player['firstName'] + " " + player['lastName'],
+            'age':
+            stat['season'] - player['born']['year'],
+            'pos':
+            pos,
+            'ovr':
+            ovr
+          })
+
+  if awardsCount == 0:
+    for player in cdla['players']:
+      if player['stats']:
+        for stat in player['stats']:
+          if stat['season'] == cdla['gameAttributes'][0]['season'] - 1 and stat[
+              'tid'] == reigningTid and stat['playoffs']:
+            pos = player['ratings'][len(player['ratings']) - 1]['pos']
+            ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+            playerChamps.append({
+              'name':
+              player['firstName'] + " " + player['lastName'],
+              'age':
+              stat['season'] - player['born']['year'],
+              'pos':
+              pos,
+              'ovr':
+              ovr
+            })
+
+  playerChamps = sorted(playerChamps, key=lambda d: d['ovr'], reverse=True)
+  return (playerChamps)
+
+
+def team0():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 0:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team1():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 1:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team2():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 2:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team3():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 3:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team4():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 4:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team5():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 5:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team6():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 6:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team7():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 7:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team8():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 8:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team9():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 9:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team10():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 10:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team11():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 11:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team12():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 12:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team13():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 13:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team14():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 14:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team15():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 15:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team16():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 16:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team17():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 17:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team18():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 18:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
+
+
+def team19():
+  players = []
+
+  for player in cdla['players']:
+    if player['tid'] == 19:
+      pos = player['ratings'][len(player['ratings']) - 1]['pos']
+      ovr = player['ratings'][len(player['ratings']) - 1]['ovr']
+      players.append({
+        'name':
+        player['firstName'] + " " + player['lastName'],
+        'age':
+        cdla['gameAttributes'][0]['season'] - player['born']['year'],
+        'pos':
+        pos,
+        'ovr':
+        ovr
+      })
+  players = sorted(players, key=lambda d: d['ovr'], reverse=True)
+  return (players)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os
 import random
-from cdla import reigning_champ, champions_list
+from cdla import reigning_champ, champions_list, team_list, reigning_champ_players, team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10, team11, team12, team13, team14, team15, team16, team17, team18, team19
 
 app = Flask(__name__)
 
@@ -13,20 +13,221 @@ imgs = ['nav_imgs/' + file for file in imgs]
 def hello_world():
   imgrand = random.sample(imgs, k=4)
   reigning = reigning_champ()
-  return render_template('home.html', imgrand=imgrand, reigning=reigning)
+  teams = team_list()
+  players = reigning_champ_players()
+  return render_template('home.html',
+                         imgrand=imgrand,
+                         reigning=reigning,
+                         teams=teams,
+                         players=players)
 
 
 @app.route("/history")
 def cdla_history():
   imgrand = random.sample(imgs, k=4)
   champs = champions_list()
-  return render_template('history.html', imgrand=imgrand, champs=champs)
+  teams = team_list()
+  return render_template('history.html',
+                         imgrand=imgrand,
+                         champs=champs,
+                         teams=teams)
 
 
 @app.route("/rules")
 def cdla_rules():
   imgrand = random.sample(imgs, k=4)
-  return render_template('rules.html', imgrand=imgrand)
+  teams = team_list()
+  return render_template('rules.html', imgrand=imgrand, teams=teams)
+
+
+@app.route("/0")
+def rio():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team0()
+  return render_template('0.html', imgrand=imgrand, teams=teams, roster=roster)
+
+
+@app.route("/1")
+def nyb():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team1()
+  return render_template('1.html', imgrand=imgrand, teams=teams, roster=roster)
+
+
+@app.route("/2")
+def phi():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team2()
+  return render_template('2.html', imgrand=imgrand, teams=teams, roster=roster)
+
+
+@app.route("/3")
+def chi():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team3()
+  return render_template('3.html', imgrand=imgrand, teams=teams, roster=roster)
+
+
+@app.route("/4")
+def mxc():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team4()
+  return render_template('4.html', imgrand=imgrand, teams=teams, roster=roster)
+
+
+@app.route("/5")
+def bkn():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team5()
+  return render_template('5.html', imgrand=imgrand, teams=teams, roster=roster)
+
+
+@app.route("/6")
+def tok():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team6()
+  return render_template('6.html', imgrand=imgrand, teams=teams, roster=roster)
+
+
+@app.route("/7")
+def was():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team7()
+  return render_template('7.html', imgrand=imgrand, teams=teams, roster=roster)
+
+
+@app.route("/8")
+def mia():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team8()
+  return render_template('8.html', imgrand=imgrand, teams=teams, roster=roster)
+
+
+@app.route("/9")
+def bei():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team9()
+  return render_template('9.html', imgrand=imgrand, teams=teams, roster=roster)
+
+
+@app.route("/10")
+def tor():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team10()
+  return render_template('10.html',
+                         imgrand=imgrand,
+                         teams=teams,
+                         roster=roster)
+
+
+@app.route("/11")
+def dal():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team11()
+  return render_template('11.html',
+                         imgrand=imgrand,
+                         teams=teams,
+                         roster=roster)
+
+
+@app.route("/12")
+def lon():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team12()
+  return render_template('12.html',
+                         imgrand=imgrand,
+                         teams=teams,
+                         roster=roster)
+
+
+@app.route("/13")
+def min():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team13()
+  return render_template('13.html',
+                         imgrand=imgrand,
+                         teams=teams,
+                         roster=roster)
+
+
+@app.route("/14")
+def sea():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team14()
+  return render_template('14.html',
+                         imgrand=imgrand,
+                         teams=teams,
+                         roster=roster)
+
+
+@app.route("/15")
+def lal():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team15()
+  return render_template('15.html',
+                         imgrand=imgrand,
+                         teams=teams,
+                         roster=roster)
+
+
+@app.route("/16")
+def lv():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team16()
+  return render_template('16.html',
+                         imgrand=imgrand,
+                         teams=teams,
+                         roster=roster)
+
+
+@app.route("/17")
+def sac():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team17()
+  return render_template('17.html',
+                         imgrand=imgrand,
+                         teams=teams,
+                         roster=roster)
+
+
+@app.route("/18")
+def cgy():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team18()
+  return render_template('18.html',
+                         imgrand=imgrand,
+                         teams=teams,
+                         roster=roster)
+
+
+@app.route("/19")
+def mem():
+  imgrand = random.sample(imgs, k=4)
+  teams = team_list()
+  roster = team19()
+  return render_template('19.html',
+                         imgrand=imgrand,
+                         teams=teams,
+                         roster=roster)
 
 
 if __name__ == "__main__":
